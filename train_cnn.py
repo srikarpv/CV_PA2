da551d658677eff918849299874e944a9e1eb614
PV SRIKAR SARMA <srikarsarma@gmail.com>
2017-11-18 13:57:23 -0500

    Add files via upload

--- train_cnn.py
+++ train_cnn.py
@@ -29,15 +29,13 @@ class ConvNet(object):
     def model_1(self, X, hidden_size):
         X1 = tf.reshape(X,[-1,784])
 
-        W = tf.Variable( tf.truncated_normal([784, hidden_size],
-                            stddev=1.0 / math.sqrt(float(784))))
+        W = tf.Variable( tf.truncated_normal([784, hidden_size],stddev=1.0 / math.sqrt(float(784))))
         b = tf.Variable(tf.zeros([hidden_size]))
-        W1 = tf.Variable(tf.truncated_normal([hidden_size,10],
-                            stddev=1.0 / math.sqrt(float(hidden_size))))
+        W1 = tf.Variable(tf.truncated_normal([hidden_size,10],stddev=1.0 / math.sqrt(float(hidden_size))))
         b1 = tf.Variable(tf.zeros([10]))
         h1 = tf.sigmoid(tf.matmul(X1, W) + b)
         y = tf.matmul(h1, W1) + b1
-        #y = tf.nn.softmax(tf.matmul(h1, W1) + b1)
+        
         
         return y
 
@@ -45,23 +43,18 @@ class ConvNet(object):
     def model_2(self, X, hidden_size):
         
         conv1 = tf.layers.conv2d(X, 20, 5, activation=tf.nn.sigmoid)
-        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
         conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
         conv2 = tf.layers.conv2d(conv1, 40, 5, activation=tf.nn.sigmoid)
-        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
         conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
         X1 = tf.reshape(conv2,[-1,640])
 
-        W = tf.Variable( tf.truncated_normal([640, hidden_size],
-                            stddev=1.0 / math.sqrt(float(640))))
+        W = tf.Variable( tf.truncated_normal([640, hidden_size],stddev=1.0 / math.sqrt(float(640))))
         b = tf.Variable(tf.zeros([hidden_size]))
-        W1 = tf.Variable(tf.truncated_normal([hidden_size,10],
-                            stddev=1.0 / math.sqrt(float(hidden_size))))
+        W1 = tf.Variable(tf.truncated_normal([hidden_size,10],stddev=1.0 / math.sqrt(float(hidden_size))))
         b1 = tf.Variable(tf.zeros([10]))
         h1 = tf.sigmoid(tf.matmul(X1, W) + b)
         y = tf.matmul(h1, W1) + b1
-        #y = tf.matmul(h1, W1) + b1
-        #y = tf.nn.softmax(tf.matmul(h1, W1) + b1)
+        
         
         return y
 
@@ -76,16 +69,13 @@ class ConvNet(object):
         conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
         X1 = tf.reshape(conv2,[-1,640])
 
-        W = tf.Variable( tf.truncated_normal([640, hidden_size],
-                            stddev=1.0 / math.sqrt(float(640))))
+        W = tf.Variable( tf.truncated_normal([640, hidden_size],stddev=1.0 / math.sqrt(float(640))))
         b = self.bias_variable([hidden_size])
-        W1 = tf.Variable(tf.truncated_normal([hidden_size,10],
-                            stddev=1.0 / math.sqrt(float(hidden_size))))
+        W1 = tf.Variable(tf.truncated_normal([hidden_size,10],stddev=1.0 / math.sqrt(float(hidden_size))))
         b1 = self.bias_variable([10])
         h1 = tf.nn.relu(tf.matmul(X1, W) + b)
         y = tf.matmul(h1, W1) + b1
-        #y = tf.matmul(h1, W1) + b1
-        #y = tf.nn.softmax(tf.matmul(h1, W1) + b1)
+        
         
         return y
 
@@ -93,50 +83,39 @@ class ConvNet(object):
     def model_4(self, X, hidden_size, decay):
         
         conv1 = tf.layers.conv2d(X, 40, 5, activation=tf.nn.relu)
-        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
         conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
         conv2 = tf.layers.conv2d(conv1, 40, 5, activation=tf.nn.relu)
-        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
         conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
         X1 = tf.reshape(conv2,[-1,640])
 
-        W = tf.Variable( tf.truncated_normal([640, hidden_size],
-                            stddev=1.0 / math.sqrt(float(640))))
+        W = tf.Variable( tf.truncated_normal([640, hidden_size],stddev=1.0 / math.sqrt(float(640))))
         b = self.bias_variable([hidden_size])
-        W1 = tf.Variable(tf.truncated_normal([hidden_size,hidden_size],
-                            stddev=1.0 / math.sqrt(float(hidden_size))))
+        W1 = tf.Variable(tf.truncated_normal([hidden_size,hidden_size],stddev=1.0 / math.sqrt(float(hidden_size))))
         b1 = self.bias_variable([hidden_size])
-        W2 = tf.Variable(tf.truncated_normal([hidden_size,10],
-                            stddev=1.0 / math.sqrt(float(hidden_size))))
+        W2 = tf.Variable(tf.truncated_normal([hidden_size,10],stddev=1.0 / math.sqrt(float(hidden_size))))
         b2 = self.bias_variable([10])
         h1 = tf.nn.relu(tf.matmul(X1, W) + b)
         h2 = tf.nn.relu(tf.matmul(h1, W1) + b1)
         y = tf.matmul(h2, W2) + b2
         weight1 = tf.nn.l2_loss(W1)
         weight2 = tf.nn.l2_loss(W2)
-        #y = tf.matmul(h1, W1) + b1
-        #y = tf.nn.softmax(tf.matmul(h1, W1) + b1)
+        
         
         return y,weight1+weight2
 
     # Use Dropout now.
     def model_5(self, X, hidden_size, is_train):
         conv1 = tf.layers.conv2d(X, 40, 5, activation=tf.nn.relu)
-        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
         conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
         conv2 = tf.layers.conv2d(conv1, 40, 5, activation=tf.nn.relu)
-        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
         conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
         X1 = tf.reshape(conv2,[-1,640])
         
-        W = tf.Variable( tf.truncated_normal([640, hidden_size],
-                            stddev=1.0 / math.sqrt(float(640))))
+        W = tf.Variable( tf.truncated_normal([640, hidden_size],stddev=1.0 / math.sqrt(float(640))))
         b = self.bias_variable([hidden_size])
-        W1 = tf.Variable(tf.truncated_normal([hidden_size,hidden_size],
-                            stddev=1.0 / math.sqrt(float(hidden_size))))
+        W1 = tf.Variable(tf.truncated_normal([hidden_size,hidden_size],stddev=1.0 / math.sqrt(float(hidden_size))))
         b1 = self.bias_variable([hidden_size])
-        W2 = tf.Variable(tf.truncated_normal([hidden_size,10],
-                            stddev=1.0 / math.sqrt(float(hidden_size))))
+        W2 = tf.Variable(tf.truncated_normal([hidden_size,10],stddev=1.0 / math.sqrt(float(hidden_size))))
         b2 = self.bias_variable([10])
         h1 = tf.nn.relu(tf.matmul(X1, W) + b)
         h1 = tf.nn.dropout(h1, 0.5)
@@ -147,10 +126,7 @@ class ConvNet(object):
         
         h_fc1_drop = tf.nn.dropout(h2, 0.5)
         y = tf.matmul(h_fc1_drop, W2) + b2
-#         weight1 = tf.nn.l2_loss(W)
-#         weight2 = tf.nn.l2_loss(W1)
-        #y = tf.matmul(h1, W1) + b1
-        #y = tf.nn.softmax(tf.matmul(h1, W1) + b1)
+#        
         
         return y,weight1+weight2
 
@@ -165,14 +141,6 @@ class ConvNet(object):
 
         trainX, trainY, testX, testY = self.read_data(train_set, test_set)
 
-
-#         b = np.zeros((trainY.shape[0], class_num))
-#         b[np.arange(trainY.shape[0]), trainY] = 1
-#         trainY = b
-
-        
-        
-
         input_size = trainX.shape[1]
         train_size = trainX.shape[0]
         test_size = testX.shape[0]
@@ -184,9 +152,7 @@ class ConvNet(object):
             # Input data
             X = tf.placeholder(tf.float32, [None, 28, 28, 1])
             Y = tf.placeholder(tf.int64, [None])
-#             depth = 10
-#             Y1 = tf.one_hot(Y, depth)
-#             print Y1.shape
+#             
 
             is_train = tf.placeholder(tf.bool)
             
